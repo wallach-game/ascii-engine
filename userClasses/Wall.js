09d9a252af1e443412efa4da1a016dbca08b7413
Jiri Korenek <jirklym@gmail.com>
2024-11-04 20:56:00 +0100

    removed

--- userClasses/Wall.js
+++
@@ -1,7 +0,0 @@
-class Wall extends GameObject {
-    constructor(x, y, symbol, collision) {
-        super(x, y, symbol,true);
-        // Additiona player-specific properties
-        this.health = 100;
-    }
-}
