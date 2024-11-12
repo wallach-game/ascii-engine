28c7610facf47944335de83407067e3b254e5f06
Jiri Korenek <jiri.korenek@optimidoc.com>
2024-11-12 16:43:11 +0100

    i dont want but okay server logic is kinda BS

--- src/CoreClasses/Editor/UI/UIGameObjectEditor.ts
+++ src/CoreClasses/Editor/UI/UIGameObjectEditor.ts
@@ -20,6 +20,7 @@ export class UIGameObjectEditor extends HTMLElement {
 <span style="color: white;">create GameObject</span>
 <input type="button" value="➕" id="createGameObject">
 <input type="button" value="🔄️" id="loadGameObject">
+<input type="button" value="💾" id="saveGameObject">
 <input type="text" id="gmSymbol" placeholder="symbol">
 <input type="text" id="gmFilter" placeholder="filter">
 <input type="color" name="" id="gmColor">
@@ -28,43 +29,72 @@ export class UIGameObjectEditor extends HTMLElement {
 </div>
 `;
 
-window.onload = () => { this.InsertClasses();};
+        window.onload = () => { this.InsertClasses(); };
     }
 
 
-InsertClasses():void {
-    console.log("classes lodaing");
-    let selector = this.shadowRoot!.getElementById("classSelector");
-    console.log(selector);
-    Editor.classes.forEach(component => {
-        let elem = document.createElement("option");
-        elem.innerText = component
-        elem.setAttribute("value", component);
-        console.log(elem);
-        selector?.appendChild(elem);
-    });
-    //this.EditorInit();
-    this.EditorInit();
-}
+    InsertClasses(): void {
+        console.log("classes lodaing");
+        let selector = this.shadowRoot!.getElementById("classSelector");
+        console.log(selector);
+        Editor.classes.forEach(component => {
+            let elem = document.createElement("option");
+            elem.innerText = component
+            elem.setAttribute("value", component);
+            console.log(elem);
+            selector?.appendChild(elem);
+        });
+        //this.EditorInit();
+        this.EditorInit();
+    }
 
-EditorInit():void{
-    this.gameObjectVizualization = this.shadowRoot!.getElementById("gameobject");
-    let symbolInput = this.shadowRoot!.getElementById("gmSymbol") as HTMLInputElement;
-    let filterInput = this.shadowRoot!.getElementById("gmFilter") as HTMLInputElement;
-    console.log(this.gameObjectVizualization);
-    let colorInput = this.shadowRoot!.getElementById("gmColor") as HTMLInputElement;
-    symbolInput!.addEventListener("change", () => { 
-        this.gameObjectVizualization!.innerText = symbolInput.value;
-        console.log(symbolInput.value.toString());
-    });
-    colorInput!.addEventListener("change", () => { 
-        this.gameObjectVizualization!.style.color = colorInput.value;
-    });
-    filterInput!.addEventListener("change", () => { 
-        this.gameObjectVizualization!.style.filter = filterInput.value;
-    });
-}
+    EditorInit(): void {
+        this.gameObjectVizualization = this.shadowRoot!.getElementById("gameobject");
+        let symbolInput = this.shadowRoot!.getElementById("gmSymbol") as HTMLInputElement;
+        let filterInput = this.shadowRoot!.getElementById("gmFilter") as HTMLInputElement;
+        let saveButton = this.shadowRoot!.getElementById("saveGameObject") as HTMLInputElement;
+        let selectorInput = this.shadowRoot!.getElementById("classSelector") as HTMLInputElement;
+        console.log(this.gameObjectVizualization);
+        let colorInput = this.shadowRoot!.getElementById("gmColor") as HTMLInputElement;
+        symbolInput!.addEventListener("change", () => {
+            this.gameObjectVizualization!.innerText = symbolInput.value;
+            console.log(symbolInput.value.toString());
+        });
+        colorInput!.addEventListener("change", () => {
+            this.gameObjectVizualization!.style.color = colorInput.value;
+        });
+        filterInput!.addEventListener("change", () => {
+            this.gameObjectVizualization!.style.filter = filterInput.value;
+        });
+        saveButton!.addEventListener("click", () => {
+            this.SaveGameObject(symbolInput.value, colorInput.value, filterInput.value, selectorInput.value);
+        });
+    }
 
+    SaveGameObject(symbol: string, color: string, filter: string, _class: string): void {
+        let gm: Object = {
+            symbol: symbol,
+            color: color,
+            filter: filter,
+            class: _class
+        };
+        console.log("saving");
+        fetch('http://localhost:3000/save-gameobject', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',  // Set the content type to JSON
+            },
+            body: JSON.stringify(gm), // Send the string and filename in the request body
+        })
+            .then(response => response.json())  // Parse the JSON response
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+    }
 }
 
 UIComponents.RegisterCustomUIComponent("ui-gameobject-editor", UIGameObjectEditor);
